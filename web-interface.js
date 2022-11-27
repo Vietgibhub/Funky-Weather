e4a6ad0b9bcaf0cfe18764b9d579b0479b173069
Viet Le <quocvietle2712@gmail.com>
2022-11-27 16:50:14 -0600

    Update web-interface.js
    keep private personal API key

--- web-interface.js
+++ web-interface.js
@@ -1,5 +1,5 @@
 let weather = {
-  apiKey: "1729b525dc136d91e7e16745986693e3",
+  apiKey: "",
   fetchWeather: function (city) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
