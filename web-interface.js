b3e2514abbbad3831e0f2d4add7ca0dfd58376da
Viet Le <quocvietle2712@gmail.com>
2022-11-27 03:01:03 -0600

    updated

--- web-interface.js
+++ web-interface.js
@@ -1,10 +1,10 @@
 let weather = {
-  apiKey: "",
+  apiKey: "1729b525dc136d91e7e16745986693e3",
   fetchWeather: function (city) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
-        "&units=metric&appid=" +
+        "&units=imperial&appid=" +
         this.apiKey
     )
       .then((response) => {
@@ -14,36 +14,37 @@ let weather = {
         }
         return response.json();
       })
-      .then((data) => this.displayWeather(data));
+      .then((data) => this.displayWeather(data)); 
   },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    document.querySelector(".city").innerText = "Weather in " + name;
+  displayWeather: function (data) { //data is the object that is returned from the API
+    const { name } = data; 
+    const { icon, description, main} = data.weather[0];
+    const { temp, humidity} = data.main; 
+    const { speed } = data.wind; 
+    document.querySelector(".city").innerText = "Weather in " + name; //display 
     document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°C";
+      "https://openweathermap.org/img/wn/" + icon + ".png"; //display 
+    document.querySelector(".description").innerText = description; //display 
+
+    document.querySelector(".temp").innerText = temp + "°F"; //display 
     document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
+      "Humidity: " + humidity + "%"; //display 
     document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
-    document.querySelector(".weather").classList.remove("loading");
-	document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + description + "')";	
-  },
+      "Wind speed: " + speed + " km/h"; //display 
+    document.querySelector(".weather").classList.remove("loading"); //remove the loading class
+	document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + main + "')";	
+  }, //display the background image followings the weather
   search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value);
+    this.fetchWeather(document.querySelector(".search-bar").value); //fetch the weather of the city that is typed in the search bar
   },
 };
 
-document.querySelector(".search button").addEventListener("click", function () {
-  weather.search();
+document.querySelector(".search button").addEventListener("click", function () { 
+  weather.search(); //when the search button is clicked, the search function is called
 });
 
 document
-  .querySelector(".search-bar")
+  .querySelector(".search-bar") //when the enter key is pressed, the search function is called
   .addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
       weather.search();
@@ -51,3 +52,13 @@ document
   });
 
 weather.fetchWeather("Lawrence");
+
+
+function myFunction() {
+  if (weather.main == "Rain"){
+window.open("rain-game/index.html", "_blank", 
+"toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=960,height=740");}
+else {
+window.open("sun-game/index.html", "_blank", 
+ "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=960,height=740");}
+}
